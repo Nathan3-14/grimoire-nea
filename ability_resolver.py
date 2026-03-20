fc1a1761548d4c4a43665aa39fbbde6ef0782c2f
Nathan <nathan.watson.172@gmail.com>
2026-03-20 18:29:07 +0000

    stuff with ability resolver

--- ability_resolver.py
+++ ability_resolver.py
@@ -1,7 +1,7 @@
 import re
 from typing import Any, List, Literal
 
-tokenise_regex = r"(?<!^)(?<![a-zA-Z0-9&*%_])(?=[a-zA-Z0-9&*%_])|(?=\+|\(|\)|\[|\]|\=\=|\>|\<)(?<!^)"
+tokenise_regex = r"(?<!^)(?<![a-zA-Z0-9&*%_])(?=[a-zA-Z0-9&*%_])|(?=\+|\(|\)|\[|\]|\=\=|-\>|\>\=|\<\=)(?<!^)"
 
 class Player:
     def __init__(self, name: str, character: str, reminders: List[str], alignment: Literal["good", "evil"]) -> None:
@@ -23,15 +23,40 @@ players = [
 def tokenise(action: str) -> List[str]:
     return re.split(tokenise_regex, action)
 
-def resolve(ability: List[str]) -> Any:
+def resolve_ability(ability: List[str]) -> Any:
     for action in ability:
-        print(tokenise(action))
+        resolve_action(action)
+
+def resolve_action(action: str) -> Any:
+    level = 0
+    index_pairs: List[List[int]] = []
+    action_tokens = tokenise(action)
+    for index, token in enumerate(action_tokens):
+        print(f"{level}: '{token}'")
+        
+        match token.strip():
+            case "(" | "[":
+                if level == 0:
+                    index_pairs.append([index]) #? stores the starting index of a set of parenthesis
+                level += 1
+
+            case ")" | "]":
+                level -= 1
+                if level == 0:
+                    index_pairs[-1].append(index) #? stores the ending index of a set of parenthesis
+
+            case _:
+                ...
+        
+    for index_pair in index_pairs:
+        resolve_action("".join(action_tokens[index_pair[0]+1:(index_pair[1])]))
+            
 
 if __name__ == "__main__":
-    resolve(["([pick(2) == (demon or &red_herring)] -> Count) >= 1"])
-    resolve(["[[self -> Neighbours] == evil] -> Count"])
-    resolve([
-        "(players == &townsfolk) -> Name",
-        "(players == &wrong) -> Name",
-        "(players == &townsfolk) -> Character"
-    ])
+    resolve_ability(["([pick(2) == (demon or &red_herring)] -> Count) >= 1"])
+    # resolve_ability(["[[self -> Neighbours] == evil] -> Count"])
+    # resolve_ability([
+    #     "(players == &townsfolk) -> Name",
+    #     "(players == &wrong) -> Name",
+    #     "(players == &townsfolk) -> Character"
+    # ])
