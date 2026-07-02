d2daf8e9fb389bcc24a48571848fe3ae3060926a
Nathan <nathan.watson.172@gmail.com>
2026-07-02 11:35:43 +0100

    changes?

--- project/scripts/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready():
-	var test = "nullnasiof"
-	if test:
-		print("yes")
-	else:
-		print("no")
--- project/scripts/unused/token_parent.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-class_name TokenHolder
-
-var current_moving: Sprite2D = null
