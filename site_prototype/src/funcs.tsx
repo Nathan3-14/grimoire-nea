da7e635c668c9535551798393e89e5d4ddb08149
Nathan <nathan.watson.172@gmail.com>
2026-08-26 21:44:24 +0200

    Generalised border checking

--- site_prototype/src/funcs.tsx
+++ site_prototype/src/funcs.tsx
@@ -1,6 +1,5 @@
 import convert from 'color-convert';
-import type { Node } from 'konva/lib/Node';
-import type { Stage } from 'konva/lib/Stage';
+import type { Settings } from './App';
 
 const TAU = 2 * Math.PI;
 export const angleFromIndex = (index: number, max: number) => {
@@ -11,33 +10,51 @@ export const goto = (address: string) => {
     window.location.href = address;
 }
 
-export const isInsideStage = (obj: Node, stage: Stage | null) => {
-    if (!stage) {return {"result": false, "direction": {"left": true, "right": true, "top": true, "bottom": true}}}
-    
-    const objBound = obj.getClientRect();
-    console.log(`${objBound.x} -> ${objBound.x+objBound.width}`)
-    console.log(`${objBound.y} -> ${objBound.y+objBound.height}`)
-    console.log(`${0} -> ${stage.width()}`)
-    console.log(`${0} -> ${stage.height()}`)
-    const left = objBound.x >= 0;
-    const right = (objBound.x + objBound.width) <= stage.width();
-    const top = objBound.y >= 0;
-    const bottom = (objBound.y + objBound.height) <= stage.height();
-    return {
-        "result": (
-            left &&
-            right &&
-            top &&
-            bottom
-        ),
-        "direction": {
-            "left": !left,
-            "right": !right,
-            "top": !top,
-            "bottom": !bottom
-        }
-    };
+export const checkCircleInsideGrim = (
+    position: {x: number, y: number},
+    diameter: number,
+    settings: Settings,
+    results: {
+        left: CallableFunction,
+        right: CallableFunction,
+        top: CallableFunction,
+        bottom: CallableFunction
+    }
+) => {
+    if (position.x < 0) {results.left()}
+    if (position.y < 0) {results.top()}
+    if ((position.x + diameter) > settings.grimWidth) {results.right()}
+    if ((position.y + diameter) > settings.grimHeight) {results.bottom()}
 }
+
+//~~ Unused?????
+// export const isInsideStage = (obj: Node, stage: Stage | null) => {
+//     if (!stage) {return {"result": false, "direction": {"left": true, "right": true, "top": true, "bottom": true}}}
+    
+//     const objBound = obj.getClientRect();
+//     console.log(`${objBound.x} -> ${objBound.x+objBound.width}`)
+//     console.log(`${objBound.y} -> ${objBound.y+objBound.height}`)
+//     console.log(`${0} -> ${stage.width()}`)
+//     console.log(`${0} -> ${stage.height()}`)
+//     const left = objBound.x >= 0;
+//     const right = (objBound.x + objBound.width) <= stage.width();
+//     const top = objBound.y >= 0;
+//     const bottom = (objBound.y + objBound.height) <= stage.height();
+//     return {
+//         "result": (
+//             left &&
+//             right &&
+//             top &&
+//             bottom
+//         ),
+//         "direction": {
+//             "left": !left,
+//             "right": !right,
+//             "top": !top,
+//             "bottom": !bottom
+//         }
+//     };
+// }
 
 export const hsl = (colour: string) => {return convert.hex.hsl(colour)};
 
