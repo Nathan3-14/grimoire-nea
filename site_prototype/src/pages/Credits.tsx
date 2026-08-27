30213d0e8ee9445188910ddf759376c13bf1ed2e
Nathan <nathan.watson.172@gmail.com>
2026-08-27 17:14:47 +0200

    Fixed credits

--- site_prototype/src/pages/Credits.tsx
+++ site_prototype/src/pages/Credits.tsx
@@ -11,7 +11,7 @@ export default function Credits({settings}: {settings: Settings}) {
         <br />
         <div className="credits-list">
             <p>This application is not affiliated with The Pandemonium Institute.<br />All roles and characters are the property of Steven Medway and The Pandemonium Institute.<br />Blood on the Clocktower is a trademark of Steven Medway and The Pandemonium Institute.</p>
-            {/* <p>Character Icons are from Tomozbot's botc-icons on <a href="https://www.github.com/tomozbot/botc-icons/">Github</a>.</p> */}
+            <p>Character Icons are from Tomozbot's botc-icons on <a href="https://www.github.com/tomozbot/botc-icons/">Github</a>.</p>
             <p>Papyrus and Old English Text MT fonts as provided by <a href="https://www.online-fonts.com">online-fonts.com</a></p>
             <p>Fredoka font provided by <a href="https://fonts.google.com/">Google Fonts</a></p>
         </div>
